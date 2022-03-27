9717841da6a87e57bb335136be27a8f8ccc8f2b4
ogatomo83 <tomoemon3022@icloud.com>
2022-03-27 21:22:12 +0900

    first commit

---
+++ go/src/work/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/input", HundlerUserFunc)
+	http.HandleFunc("/view", HandlerUserConfirm)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+func HundlerUserFunc(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseFiles("template/input.html"))
+
+	values := map[string]string{}
+
+	if err := tpl.ExecuteTemplate(w, "input.html", values); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func HandlerUserConfirm(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseFiles("template/view.html"))
+
+	values := map[string]string{
+		"name":    r.FormValue("name"),
+		"comment": r.FormValue("comment"),
+	}
+
+	fmt.Println(values)
+
+	if err := tpl.ExecuteTemplate(w, "view.html", values); err != nil {
+		fmt.Println(err)
+	}
+}
